3a05a2c9ca811d5112ee4517d0ec09cc2fa07deb
Tr3mo89 <mail@tr3mo89.eu>
2021-10-31 22:20:55 +0000

    task6: output to kubernetes file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,6 @@ dockerpath="tr3mo89/udacity-project-5"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-# kubectl run flask --image="tr3mo89/udacity-project-5" --port=8000
 kubectl run flask --image=$dockerpath --port=8000
 
 # Step 3:
@@ -18,5 +17,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-#kubectl expose pod flask --port=8000 --type="NodePort" --target-port=80
 kubectl port-forward flask 8000:80
