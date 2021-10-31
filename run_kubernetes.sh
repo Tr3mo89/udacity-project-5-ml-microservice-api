41f4d52b37ccd9ef240babd1412e1c7d6e81987e
Tr3mo89 <mail@tr3mo89.eu>
2021-10-31 22:15:17 +0000

    tasl 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="tr3mo89/udacity-project-5"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# kubectl run flask --image="tr3mo89/udacity-project-5" --port=8000
+kubectl run flask --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+#kubectl expose pod flask --port=8000 --type="NodePort" --target-port=80
+kubectl port-forward flask 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="$tr3mo89/udacity-project-5"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u $dockerId -p $dockerAuth
+docker tag udacity/flask:latest $dockerpath:latest
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
